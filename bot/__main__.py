9769845ffb77f59c1af9b4fc39a86f64fcc9f58e
NFS86 <jarbull86@gmail.com>
2022-07-07 13:17:24 +0000

    change ping msg

--- bot/__main__.py
+++ bot/__main__.py
@@ -96,7 +96,7 @@ def ping(update, context):
     start_time = int(round(time() * 1000))
     reply = sendMessage("Starting Ping", context.bot, update.message)
     end_time = int(round(time() * 1000))
-    editMessage(f'<b>🖥 Server {end_time - start_time} MilliSecond 🖥</b>', reply)
+    editMessage(f'<b>🕹 {end_time - start_time} ms </b>', reply)
 
 
 def log(update, context):
